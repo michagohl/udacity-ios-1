356d2f284f00c971d0021deda92259497672d551
Michael Gohl <micha@gohl.tk>
2018-04-26 15:25:48 +0200

    Initial Commit

---
+++ pitchPerfect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pitchPerfect
+//
+//  Created by Michael Gohl on 26.04.18.
+//  Copyright © 2018 Michael Gohl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
